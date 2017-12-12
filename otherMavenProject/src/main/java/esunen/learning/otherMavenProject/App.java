ef1b5831a28cf5bda8feaf9d00a0100853e8990c
Eduard Suñén Pinyol <esunen@gmail.com>
2017-12-12 15:18:09 +0100

    First commit in learning.

---
+++ otherMavenProject/src/main/java/esunen/learning/otherMavenProject/App.java
@@ -0,0 +1,13 @@
+package esunen.learning.otherMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
